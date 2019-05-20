589aad1ddd91c8a7be3d9de90c162fbbabae9cf6
Giacomo Mazzamuto <mazzamuto@lens.unifi.it>
2019-05-20 11:39:14 +0200

    gui: cmake: add option to force fusion style

--- src/gui/main.cpp
+++ src/gui/main.cpp
@@ -1,4 +1,5 @@
 #include <QApplication>
+#include <QStyleFactory>
 
 #include "mainwindow.h"
 #include "version.h"
@@ -10,6 +11,11 @@ int main(int argc, char *argv[])
     QCoreApplication::setApplicationName(PROGRAM_NAME);
 
     QApplication a(argc, argv);
+
+#ifdef FORCE_FUSION_STYLE
+    a.setStyle(QStyleFactory::create("fusion"));
+#endif
+
     MainWindow w;
     w.show();
 
