9da3a263b6ec209f702c56c759afc78bd2e8668a
spim <spim@localhost>
2020-07-03 16:20:46 +0200

    increase buffer size

--- src/core/spim.cpp
+++ src/core/spim.cpp
@@ -105,7 +105,7 @@ void SPIM::initialize()
                                    DCAM::DCAMPROP_READOUT_DIRECTION__FORWARD);
             orca->setPropertyValue(
                 DCAM::DCAM_IDPROP_OUTPUTTRIGGER_PREHSYNCCOUNT, 0);
-            orca->buf_alloc(100);
+            orca->buf_alloc(1000);
         }
 
         for (int devnumber = 1; devnumber <= 16; ++devnumber) {
