6fbcd6220e656fb3332f470b66d44bfc1e9e31a2
Ramortada <123553880+Ramortada@users.noreply.github.com>
2023-07-28 14:36:56 +0200

    Update galvosWidget.cpp

--- src/gui/galvosWidget.cpp
+++ src/gui/galvosWidget.cpp
@@ -13,102 +13,104 @@ galvosWidget::galvosWidget(QWidget *parent)
     setupUI();
 }
 
+galvoswidget::widgetboxes(galvoRamp *something)
+{
+        int row = 0;
+        int col = 0;
+        QVector<double> wp = something->getWaveformParams();
+        wp = wp.mid(i * GALVORAMP_N_OF_PARAMS, GALVORAMP_N_OF_PARAMS);
+
+        DoubleSpinBox *offsetSpinBox = new DoubleSpinBox();
+        offsetSpinBox->setRange(-10, 10);
+        offsetSpinBox->setDecimals(3);
+        offsetSpinBox->setSingleStep(0.01);
+        offsetSpinBox->setSuffix(" V");
+        offsetSpinBox->setValue(wp.at(GALVORAMP_OFFSET_IDX));
+        grid->addWidget(offsetSpinBox, row, col++);
+
+        DoubleSpinBox *amplitudeSpinBox = new DoubleSpinBox();
+        amplitudeSpinBox->setRange(-10, 10);
+        amplitudeSpinBox->setDecimals(3);
+        amplitudeSpinBox->setSingleStep(0.01);
+        amplitudeSpinBox->setSuffix(" V");
+        amplitudeSpinBox->setValue(wp.at(GALVORAMP_AMPLITUDE_IDX));
+        grid->addWidget(amplitudeSpinBox, row, col++);
+
+        DoubleSpinBox *delaySpinBox = new DoubleSpinBox();
+        delaySpinBox->setRange(-1000, 1000);
+        delaySpinBox->setDecimals(3);
+        delaySpinBox->setSingleStep(0.05);
+        delaySpinBox->setSuffix(" ms");
+        delaySpinBox->setValue(wp.at(GALVORAMP_DELAY_IDX) * 1000);
+        grid->addWidget(delaySpinBox, row, col++);
+
+        DoubleSpinBox *fractionSpinBox = new DoubleSpinBox();
+        fractionSpinBox->setRange(0, 100);
+        fractionSpinBox->setDecimals(2);
+        fractionSpinBox->setSingleStep(0.1);
+        fractionSpinBox->setSuffix(" %");
+        fractionSpinBox->setValue(wp.at(GALVORAMP_FRACTION_IDX) * 100);
+        grid->addWidget(fractionSpinBox, row, col++);
+
+        std::function<void(void)> apply = [=]() {
+            something->setWaveformAmplitude(i, amplitudeSpinBox->value());
+            something->setWaveformOffset(i, offsetSpinBox->value());
+            something->setWaveformDelay(i, delaySpinBox->value() / 1000.);
+            something->setWaveformRampFraction(i, fractionSpinBox->value() / 100.);
+            something->updateWaveform();}
+        
+        connect(offsetSpinBox, &DoubleSpinBox::returnPressed, this, apply);
+        connect(amplitudeSpinBox, &DoubleSpinBox::returnPressed, this, apply);
+        connect(delaySpinBox, &DoubleSpinBox::returnPressed, this, apply);
+        connect(fractionSpinBox, &DoubleSpinBox::returnPressed, this, apply);
+}
+
 galvosWidget::setupUI()
 {
     QGridLayout *grid = new QGridLayout();
     int row = 0;
-    grid->addWidget(new QLabel("Fourier plane galvo pair"), row, 0, 1, 1);
+    int col = 0;
+    GalvoRamp *smth;
+    
+    grid->addWidget(new QLabel("Offset"), row, col);
+    grid->addWidget(new QLabel("Amplitude"), row, col++);
+    grid->addWidget(new QLabel("Delay"), row, col++);
+    grid->addWidget(new QLabel("Fraction"), row, col++);
+    
+    grid->addWidget(new QLabel("Fourier plane galvo pair"), row++, 0, 1, 1); 
     grid->addWidget(new QLabel("Generating Light Sheet"), row++, 0, 1, 1);
-    grid->addWidget(new GalvoWaveformWidget(), row++, 0, 1, 1);  //x2
-
-    list of strings titles= ["Diagonal correction", "Rapid correction", "Inclination correction"];
-    for (int j=0;j<size(titles) ;j++){
-        grid->addWidget(new QLabel(titles[j]), row++, 0, 1, 1);
+    grid->addWidget(new GalvoWaveformWidget(), row++, 0, 1, 1);  //Reusing the same class for two mirrors of G2
     
-        GalvoRamp *smth = new GalvoRamp;
-        smth->spim().correctionGalvos[j];
-        
-        //GalvoRamp *gr = spim().getTasks()->getGalvoRamp();
+    grid->addWidget(new QLabel("Diagonal correction waveform"), row++, 0, 1, 1); //the second mirror of G2
+    for (int j = 0; j<SPIM_NCAMS; j++){
+        smth = spim().getCorrectionGalvos[j];  //we first point to the two mirrors of g2
+        widgetboxes(smth); }
+
+    grid->addWidget(new QLabel("Image plane galvo pair"), row++, 0, 1, 1); //now the G1 galvos
     
-        int col = 0;
-        grid->addWidget(new QLabel("Offset"), row++, col++);
-        grid->addWidget(new QLabel("Amplitude"), row, col++);
-        grid->addWidget(new QLabel("Delay"), row, col++);
-        grid->addWidget(new QLabel("Fraction"), row, col++);
-        row++;
-            for (int i = 0; i < SPIM_NCAMS; ++i) {
-                col = 0;
-                //QVector<double> wp = gr->getWaveformParams();
-                QVector<double> wp = smth->getWaveformParams();
-                wp = wp.mid(i * GALVORAMP_N_OF_PARAMS, GALVORAMP_N_OF_PARAMS);
+    grid->addWidget(new QLabel("Rapid beta 1 correction"), row++, 0, 1, 1); //the two mirrors of g1 we move thanks to with RAPID
+    for (int j = 2; j<SPIM_NCAMS; j++){
+        smth = spim().getCorrectionGalvos[j];  
+        widgetboxes(smth); }
+    
+    grid->addWidget(new QLabel("Descan inclination correction"), row++, 0, 1, 1); //the two mirrors of g1 we move thanks to g3's descan inclination
+    for (int j = 4; j<SPIM_NCAMS; j++){
+        smth = spim().getCorrectionGalvos[j];  
+        widgetboxes(smth); }    
 
-                DoubleSpinBox *offsetSpinBox = new DoubleSpinBox();
-                offsetSpinBox->setRange(-10, 10);
-                offsetSpinBox->setDecimals(3);
-                offsetSpinBox->setSingleStep(0.01);
-                offsetSpinBox->setSuffix(" V");
-                offsetSpinBox->setValue(wp.at(GALVORAMP_OFFSET_IDX));
-                grid->addWidget(offsetSpinBox, row, col++);
+    grid->addWidget(new QLabel("Descanning galvo "), row++, 0, 1, 1); //G3's parameters
+    smth = spim().getCorrectionGalvos[6];  
+    widgetboxes(smth);  
 
-                DoubleSpinBox *amplitudeSpinBox = new DoubleSpinBox();
-                amplitudeSpinBox->setRange(-10, 10);
-                amplitudeSpinBox->setDecimals(3);
-                amplitudeSpinBox->setSingleStep(0.01);
-                amplitudeSpinBox->setSuffix(" V");
-                amplitudeSpinBox->setValue(wp.at(GALVORAMP_AMPLITUDE_IDX));
-                grid->addWidget(amplitudeSpinBox, row, col++);
-
-                DoubleSpinBox *delaySpinBox = new DoubleSpinBox();
-                delaySpinBox->setRange(-1000, 1000);
-                delaySpinBox->setDecimals(3);
-                delaySpinBox->setSingleStep(0.05);
-                delaySpinBox->setSuffix(" ms");
-                delaySpinBox->setValue(wp.at(GALVORAMP_DELAY_IDX) * 1000);
-                grid->addWidget(delaySpinBox, row, col++);
-
-                DoubleSpinBox *fractionSpinBox = new DoubleSpinBox();
-                fractionSpinBox->setRange(0, 100);
-                fractionSpinBox->setDecimals(2);
-                fractionSpinBox->setSingleStep(0.1);
-                fractionSpinBox->setSuffix(" %");
-                fractionSpinBox->setValue(wp.at(GALVORAMP_FRACTION_IDX) * 100);
-                grid->addWidget(fractionSpinBox, row, col++);
-
-                std::function<void(void)> apply = [=]() {
-                    smth->setWaveformAmplitude(i, amplitudeSpinBox->value());
-                    smth->setWaveformOffset(i, offsetSpinBox->value());
-                    smth->setWaveformDelay(i, delaySpinBox->value() / 1000.);
-                    smth->setWaveformRampFraction(i, fractionSpinBox->value() / 100.);
-                    smth->updateWaveform();
-                /*
-                gr->setWaveformAmplitude(i, amplitudeSpinBox->value());
-                gr->setWaveformOffset(i, offsetSpinBox->value());
-                gr->setWaveformDelay(i, delaySpinBox->value() / 1000.);
-                gr->setWaveformRampFraction(i, fractionSpinBox->value() / 100.);
-                gr->updateWaveform();
-                */
-                };
-
-                connect(offsetSpinBox, &DoubleSpinBox::returnPressed, this, apply);
-                connect(amplitudeSpinBox, &DoubleSpinBox::returnPressed, this, apply);
-                connect(delaySpinBox, &DoubleSpinBox::returnPressed, this, apply);
-                connect(fractionSpinBox, &DoubleSpinBox::returnPressed, this, apply);
-                
-                delete smth;
-                row++;
-            }
-    }
-}
-        
-/*
-    list of strings main_titles = ["Fourier plane galvo pair","Image plane galvo pair","Descan galvo"];
-    QGroupBox *gbox = new QGroupBox(main_titles[h]);
+    QGroupBox *gbox = new QGroupBox("Galvo Ramp");
     gbox->setLayout(grid);
 
     QBoxLayout *layout = new QHBoxLayout();
     layout->addWidget(gbox);
     setLayout(layout);
-*/
+}
+        
+
     
 
 
